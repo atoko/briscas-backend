2f7dacb8e354959bc11fb966990e98ee760a6be0
Pedro Cardona <pedro@evermagica.com>
2016-11-07 16:31:17 -0500

    monkeypatching

--- src/routes/game/play.js
+++ src/routes/game/play.js
@@ -1,6 +1,7 @@
 import brisca from "../../lib/brisca";
 
 let play = function(req, res, next) {
+	debugger;
 	let reqCard = req.params.card;
 	let gameData = req.game.data;
 	let b = brisca.deserialize(gameData);
@@ -11,8 +12,6 @@ let play = function(req, res, next) {
 		res.json(req.game).flush();
 	} else {
 		req.game.data = b.serialize();
-
- +		req.game.player_data = null;
  +		delete req.game.player_data;		
 		req.db.brisca.games.save(req.game, function(err, row) {
 			if (err) {
